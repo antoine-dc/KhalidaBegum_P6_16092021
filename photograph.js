56b409372077a0cb9aee591103eda217c6f2ed94
Khalida Begum <k.begum1@gmail.com>
2021-11-02 12:23:19 +0100

    update

--- photograph.js
+++ photograph.js
@@ -133,16 +133,8 @@ loadData().then((data) => {
   }
 });
 
-//sort menu
-
-//const sortContent = document.querySelector(".sort-content");
-const dropBtn = document.querySelectorAll(".dropbtn");
-
-//const dropMenu= document.getElementById("#dropMenu");
-////const modalBackground = document.querySelector(".background");
-//const modalBtn = document.querySelectorAll(".dropbtn");
-
 // Launch sort menu
+const dropBtn = document.querySelectorAll(".dropbtn");
 
 dropBtn.forEach((btn) => btn.addEventListener("click", sortMenu));
 
@@ -150,65 +142,134 @@ function sortMenu() {
   dropMenu.style.display = "block";
 }
 
-//const closeContent = dropBtn.querySelector();
-const closeContent = document.getElementById("dropMenu");
-window.onclick = function (event) {
-  if (event.target == closeContent) {
-    dropMenu.style.display = "none";
-  }
-};
-/*
-btn.addEventListener("click", closeContent);
-function closeContent() {
+const contentClose = document.querySelector(".sort-content");
+contentClose.addEventListener("click", closeMenu);
+
+function closeMenu() {
   dropMenu.style.display = "none";
-}*/
+}
+
+// Diaporama
 /*
-let i;
-for (i = 0; i < dropdowns.length; i++) {
-  let openDropdown = dropdowns[i];
-  if (openDropdown.classList.contains("show")) {
-    openDropdown.classList.remove("show");
-  }
-}*/
+const lightboxPics = document.querySelectorAll(".portfolio-pics");
+
+lightboxPics.forEach((image) => {
+  image.addEventListener("click", e => {
+    if (e.target !== e.currentTarget)return
+    const img = document.createElement("img");
+    img.src = image.src;
+    lightbox.appendChild(img);
+  });
+});
+*/
+/*
+const nbSlide = diaporama.length;
+
+const lightboxPics = (media) => {
+  return convertStringToHTML(`
+<div class="lightbox" id="lightbox">
+        <span class="lightbox-close"></span>
+
+        <div class="nav-arrow">
+          <button
+            id="nav-left"
+            class="fas fa-arrow-left"
+            onclick="prevImage()"
+          ></button>
+          <button
+            id="nav-right"
+            class="fas fa-arrow-right"
+            onclick="nextImage()"
+          ></button>
+        </div>
+        <div class="lightbox-container">
+          <div class="lightbox-image">
+            <img
+              src="Sample Photos/${media.photographerId}/${media.image}"
+              alt="${media.title}"
+            />
+          </div>
+          <div class="lightbox-text">
+            <span> ${media.title}</span>
+          </div>
+        </div>
+        </div>`);
+};*/
 
 /*
-//lightbox
-const lightbox = document.querySelector(".portfolio");
-const diaporama = document.querySelectorAll(".portfolio-pics");
 
-
-
-let compteur = 0; // Compteur qui permettra de savoir sur quelle slide nous sommes
+//slider arrows
+const next = document.querySelector("#nav-right");
+const prev = document.querySelector("#nav-left");
+let count = 0; // Compteur permettra de savoir sur quelle slide nous sommes
 
 function slideNext() {
-  // On incrémente le compteur
-  compteur++;
-
-  // Si on dépasse la fin du diaporama, on "rembobine"
-  if (compteur == slides.length) {
-    compteur = 0;
+  // Incrémente le compteur
+  count++;
+  if (count == nbSlide) {
+    // Si on dépasse la fin du diaporama, on "rembobine"
+    count = 0;
   }
 }
+next.addEventListener("click", slideNext);
 
-/**
- * Cette fonction fait défiler le diaporama vers la gauche
- */
-/*
 function slidePrev() {
-  // On décrémente le compteur
-  compteur--;
+  //Décrémente le compteur
+  count--;
+  if (count < 0) {
+    // Si on dépasse le début du diaporama, on repart à la fin
+    count = nbSlide - 1;
+  }
+}
+prev.addEventListener("click", slidePrev);
 
-  // Si on dépasse le début du diaporama, on repart à la fin
-  if (compteur < 0) {
-    compteur = slides.length - 1;
+class lightbox {
+  constructor(element, options = {}) {
+    this.element = element;
+    this.options = Object.assign(
+      {},
+      {
+        slidesToScroll: 1,
+        slidesVisible: 1,
+      },
+      options
+    );
+    this.children = [].slice.call(element.children);
+    let root = this.createDivWithClass("lightbox");
+    let container = this.createDivWithClass("lightbox-container");
+    root.appendChild(container);
+    this.element.appendChild(root);
+    this.children.forEach(function (child) {
+      container.appendChild(child);
+    });
+  }
+  createDivWithClass(className) {
+    let div = document.createElement("div");
+    div.setAttribute("class", className);
+    return div;
   }
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+  new lightbox(document.querySelector("#lightbox"), {
+    slidesToScroll: 1,
+    slidesVisible: 1,
+  });
+});
 */
-
 /*
-class Lightbox {
-  static init() {
+
+
+
+
+class lightbox  {
+  constructor (element, options = {})
+};
+document.addEventListener('DOMContentLoaded, function(){
+
+})
+/*
+ static init() {
     const links = document
       .querySelectorAll('img[src$=".jpg"],video[src$=".mp4"]')
       .forEach((link) =>
@@ -217,18 +278,7 @@ class Lightbox {
           new Lightbox(e.currentTarget.getAttribute("src"));
         })
       );
-  }
-}*/
-
-//slider arrows
-const next = document.querySelector("#nav-right");
-const prev = document.querySelector("#nav-left");
-next.addEventListener("click", slideNext);
-prev.addEventListener("click", slidePrev);
-
-/*const lightbox = document.querySelector(".portfolio");
-const diaporama = document.querySelectorAll(".portfolio-pics");
-diaporama.addEventListener("click",lightboxPics);*/
+  }*/
 
 /*function afficheImage() {
   return `<button><div class="lightbox-close"></div></button>
@@ -325,3 +375,71 @@ class Lightbox {
   }
 }
 */
+
+const lightboxImage = document.querySelectorAll(".portfolio-pics");
+const next = document.querySelector("#nav-right");
+const prev = document.querySelector("#nav-left");
+const current = 0;
+
+function reset() {
+  for (let i = 0; i < lightboxImage.length; i++) {
+    lightboxImage[i].style.display = "none";
+  }
+}
+
+function startSlide() {
+  reset();
+  lightboxImage[0].style.display = "block";
+}
+
+function slideLeft() {
+  reset();
+  lightboxImage[current - 1];
+  current--;
+}
+
+function slideRight() {
+  reset();
+  lightboxImage[current + 1];
+  current++;
+}
+
+prev.addEventListener("click", function () {
+  if (current === 0) {
+    current = lightboxImage.length;
+  }
+  slideLeft();
+});
+
+next.addEventListener("click", function () {
+  if (current === lightboxImage.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+let image = ".portfolio-pics";
+let i = image.length;
+
+function nextImage() {
+  if (i > image.length) {
+    i = i + 1;
+  } else {
+    i = 1;
+  }
+  sliderContent.innerHTML = "<img src=" + image[i - 1] + ".jpg>";
+}
+function prevImage() {
+  if (i < image.length + 1 && i > 1) {
+    i = i - 1;
+  } else {
+    i = image.length;
+  }
+  sliderContent.innerHTML = "<img src=" + image[i - 1] + ".jpg>";
+}
+const links = document.querySelectorAll("portfolio-pics").forEach((link) =>
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    new links(e.currentTarget.getAttribute("src"));
+  })
+);
