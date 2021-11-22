25b88ae5455baf4b4f3d37f63eca78d07cbf1609
Khalida Begum <k.begum1@gmail.com>
2021-11-22 14:07:14 +0100

    code plus propre

--- photograph.js
+++ photograph.js
@@ -4,14 +4,106 @@ import { loadData, convertStringToHTML } from "./common.js";
 const modalBackground = document.querySelector(".background");
 const modalBtn = document.querySelectorAll(".btn-contact");
 const lightboxBackground = document.querySelector(".lightbox");
-
-// Launch Modal
-
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+const next = document.querySelector("#nav-right");
+const prev = document.querySelector("#nav-left");
+let containerMedia = document.querySelector(".lightbox-image");
+const closeBox = lightboxBackground.querySelector(".lightbox-close");
+closeBox.addEventListener("click", closeLightbox);
 const urlPhotographerId = +new URLSearchParams(location.search).get(
   "photographerId"
 );
+// Launch sort menu
+const dropBtn = document.querySelectorAll(".close-menu");
+
+const contentClose = document.querySelector(".dropbtn");
+contentClose.addEventListener("click", closeMenu);
+
+const closeBtn = modalBackground.querySelector(".close");
+closeBtn.addEventListener("click", closeModal);
+
+//photographer bio
+
+const getPhotographerBio = (photographer) => {
+  return convertStringToHTML(`
+  <div class="profile-text">
+    <h1>${photographer.name}</h1>
+  
+    <span>${photographer.city}, ${photographer.country}</span>
+    <p>${photographer.tagline}</p>
+  </div>
+  <span class="profile-pic">
+    <img
+      src="Sample Photos/Photographers ID Photos/${photographer.portrait}"
+      alt="Portrait de ${photographer.name}"
+    />
+  </span>
+  
+`);
+};
+
+const getTags = (photographer) => {
+  return convertStringToHTML(
+    photographer.tags
+      .map((tag) => {
+        return `<span> <li><a href="index.html?tag=${tag}">#${tag}</a></li></span> `;
+      })
+      .join("")
+  );
+};
+
+const getVideo = (media, index) => {
+  return `  
+  <article class="portfolio-pics">
+   <div>
+   <a href="#">
+     <video controls data-id="${index}">
+       <source
+         src="Sample Photos/${media.photographerId}/${media.video}"
+         type="Video" alt="${media.title}"
+       />
+     </video>
+     </a>
+<div class="portfolio-text" data-id="${index}">
+<a href="photograph.html?title=${media.title}"> <span class="titles">${media.title}</span></a>
+<a href="photograph.html?likes=${media.likes}"> <span class="portfolio-likes">${media.likes} ❤</span> </a> 
+     </div>
+   </div>
+ </article>
+`;
+};
+
+const getImage = (media, index) => {
+  return `
+    <article class="portfolio-pics">
+      <div>
+        <a href="#"><img 
+          src="Sample Photos/${media.photographerId}/${media.image}"
+          alt="${media.title}"
+          data-id="${index}"
+        /></a>
+        <div class="portfolio-text" data-id="${index}">
+       <a href="photograph.html?title=${media.title}"> <span class="titles">${media.title}</span></a>
+       <a href="photograph.html?likes=${media.likes}"> <span class="portfolio-likes">${media.likes} ❤</span> </a>  
+        </div> 
+      </div>
+    </article>
+  `;
+};
+
+const getMedia = (media, index) => {
+  if (media.video) {
+    return getVideo(media, index);
+  }
+  return getImage(media, index);
+};
+
+const getMedias = (medias) => {
+  return convertStringToHTML(
+    medias.map((media, index) => getMedia(media, index)).join("")
+  );
+};
+
+// Launch Modal
 
 loadData().then((data) => {
   const photographer = data.photographers.find((photographer) => {
@@ -46,15 +138,6 @@ loadData().then((data) => {
     })
   );
 
-  const closeBox = lightboxBackground.querySelector(".lightbox-close");
-
-  closeBox.addEventListener("click", closeLightbox);
-
-  const next = document.querySelector("#nav-right");
-  const prev = document.querySelector("#nav-left");
-
-  let containerMedia = document.querySelector(".lightbox-image");
-
   next.addEventListener("click", () => {
     count = parseInt(count) + 1;
 
@@ -76,18 +159,8 @@ loadData().then((data) => {
   });
 });
 
-// Launch sort menu
-const dropBtn = document.querySelectorAll(".close-menu");
-
-dropBtn.forEach((btn) => btn.addEventListener("click", sortMenu));
-
-const contentClose = document.querySelector(".dropbtn");
-contentClose.addEventListener("click", closeMenu);
-
-const closeBtn = modalBackground.querySelector(".close");
-closeBtn.addEventListener("click", closeModal);
-
 // Functions
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 function closeLightbox() {
   lightboxBackground.style.display = "none";
@@ -114,109 +187,17 @@ function sortMenu() {
 function closeMenu() {
   dropMenu.style.display = "none";
 }
+dropBtn.forEach((btn) => btn.addEventListener("click", sortMenu));
 
-//photographer bio
+//sort
 
-const getPhotographerBio = (photographer) => {
-  return convertStringToHTML(`
-  <div class="profile-text">
-    <h1>${photographer.name}</h1>
-  
-    <span>${photographer.city}, ${photographer.country}</span>
-    <p>${photographer.tagline}</p>
-  </div>
-  <span class="profile-pic">
-    <img
-      src="Sample Photos/Photographers ID Photos/${photographer.portrait}"
-      alt="Portrait de ${photographer.name}"
-    />
-  </span>
-  
-`);
-};
+const urlLikes = +new URLSearchParams(location.search).get("likes");
+const byPopularity = document.querySelector(".sort-popularity");
+const numberOfLikes = [urlLikes];
+numberOfLikes.sort();
+console.log(numberOfLikes);
+byPopularity.addEventListener("click", numberOfLikes);
 
-const getTags = (photographer) => {
-  return convertStringToHTML(
-    photographer.tags
-      .map((tag) => {
-        return `<span> <li><a href="#">#${tag}</a></li></span> `;
-      })
-      .join("")
-  );
-};
-
-const getVideo = (media, index) => {
-  return `  
-  <article class="portfolio-pics">
-   <div>
-   <a href="#">
-     <video controls data-id="${index}">
-       <source
-         src="Sample Photos/${media.photographerId}/${media.video}"
-         type="Video" alt="${media.title}"
-       />
-     </video>
-     </a>
-<div class="portfolio-text" data-id="${index}">
-     <span class="title">${media.title}</span>
-     <span class="portfolio-likes">${media.likes} ❤</span>
-     </div>
-   </div>
- </article>
-`;
-};
-const getImage = (media, index) => {
-  return `
-    <article class="portfolio-pics">
-      <div>
-        <a href="#"><img 
-          src="Sample Photos/${media.photographerId}/${media.image}"
-          alt="${media.title}"
-          data-id="${index}"
-        /></a>
-        <div class="portfolio-text" data-id="${index}">
-        <span class="title">${media.title}</span>
-        <span class="portfolio-likes">${media.likes} ❤</span>   
-        </div> 
-      </div>
-    </article>
-  `;
-};
-
-const getMedia = (media, index) => {
-  if (media.video) {
-    return getVideo(media, index);
-  }
-  return getImage(media, index);
-};
-
-const getMedias = (medias) => {
-  return convertStringToHTML(
-    medias.map((media, index) => getMedia(media, index)).join("")
-  );
-};
-
-//const like = new URLSearchParams(getLikes);
-
-//byPopularity.addEventListener("click", sortedLikes);
-//console.log(like);
-
-//const getLikes = +new URLSearchParams(location.search).value('likes');
-//const titles = new URLSearchParams(window.location.search).sort(['title']);
-//const titles = new URLSearchParams(window.location.search).get("title");
-//const title = "$.title";
-/*const titles = ["a", "c", "b", "e", "d"];
-const sortedTitles = titles.sort((a, b) => {
-  if (a < b) {
-    return -1;
-  } else if (a > b) {
-    return 1;
-  } else {
-    return 0;
-  }
-});
-
-byTitle.addEventListener("click", sortedTitles);
-
-console.log(sortedTitles);
-*/
+let numbers = [4, 2, 5, 1, 3];
+numbers.sort((a, b) => a - b);
+console.log(numbers);
--- sort.js
+++ sort.js
@@ -9,263 +9,78 @@ portfolio-pic title = media title
 - logLikes
 - click popular to display in order of numberOfLikes*/
 
-const byPopularity = [media.likes];
-byPopularity.sort();
-console.log(byPopularity);
-
+//Sort
+//get button and add event listener on click
 const byPopularity = document.querySelector(".sort-popularity");
-const like = +new URLSearchParams(window.location.search).get("likes");
+const byTitle = document.querySelector(".sort-title");
+const byDate = document.querySelector(".sort-date");
+const likes = document.querySelector[".portfolio-likes"];
+const titles = document.querySelector(".titles");
 
-const getLikes = (media) => {
+byPopularity.addEventListener("click");
+byTitle.addEventListener("click");
+byDate.addEventListener("click");
+
+//-add parameter to url
+//- add a href
+
+//-recover info from json ie likes+results/title+results
+const urlLikes = +new URLSearchParams(location.search).get("likes");
+const urlTitles = new URLSearchParams(location.search).get("title");
+const urlDate = new URLSearchParams(location.search).get("date");
+
+// display results
+
+/*
+const like = data.media.likes((media) => {
+  return data.media.likes === urlLikes;
+});*/
+
+/*const getLikes = (media) => {
   return convertStringToHTML(
     media.likes
       .map((media) => {
-        return `<span class="portfolio-likes">${media.likes} ❤</span>  `;
+        return `<a href="photograph.html?likes=${media.likes}">
+          <span class="portfolio-likes">${media.likes} ❤</span>
+        </a>`;
       })
       .join("")
   );
-};
+};*/
+//byPopularity.addEventListener("click", );
 
-loadData().then((data) => {
-  if (like) {
-    return data.media.sort((media) => media.likes.includes(like));
-  }
-  return data.media;
+byPopularity.addEventListener("click", () => {
+  return media.likes;
 });
 
-const byTitle = document.querySelector(".title");
-const urllikes = +new URLSearchParams(location.search).get("likes");
+//console.log(media.likes);
 
-const sortList = ["likes", "title", "date"];
-const numberOfLikes = ["likes"];
-function sortLikes(a, b) {
-  if (a.likes > b.likes) {
-    return 1;
-  } else if (b.likes > a.likes) {
-    return -1;
-  } else {
-    return 0;
-  }
-}
+const titleSort = function sortByTitle(a, b) {
+  if (a > b) return 1;
+  if (b > a) return -1;
 
-function byLikes(a, b) {
-  if (a.likes > b.likes) {
-    return 1;
-  } else if (b.likes > a.likes) {
-    return -1;
-  } else {
-    return 0;
-  }
-}
-
-function byDate() {
-  return new Date(a.date).valueOf() - new Date(b.date).valueOf();
-}
-
-const numberOfLikes = [
-  {
-    media: likes,
-  },
-];
-
-const sortPopularity = document.querySelector(".sort-popularity");
-sortPopularity.sort("likes");
-console.log(sortPopularity);
-
-const like = new URLSearchParams(window.location.search).get("likes");
-loadData().then((data) => {
-  if (like) {
-    return data.media.sort((media) => media.likes.includes(like));
-  }
-  return data.media;
-});
-
-const getLikes = (media) => {
-  return convertStringToHTML(
-    media.likes
-      .map((media) => {
-        return `<span class="portfolio-likes">${media.likes} ❤</span>  `;
-      })
-      .join("")
-  );
+  return title.sort(title);
 };
+byTitle.addEventListener("click", titleSort);
 
-function displayLikes(media, likes) {
-  return sortPopularity;
-}
-
-sortPopularity.addEventListener("click", getLikes);
-
-const like = new URLSearchParams(window.location.search).get("likes");
-const numberOfLikes = document.querySelector(".sort-popularity");
-
-const getLikes = (media) => {
-  return convertStringToHTML(
-    media.likes
-      .map((media) => {
-        return `<span class="portfolio-likes">${media.likes} ❤</span>  `;
-      })
-      .join("")
-  );
-};
-
-loadData().then((data) => {
-  if (like) {
-    return data.media.sort((media) => media.likes.includes(like));
-  }
-  return data.media;
+byPopularity.addEventListener("click", (getLikes) => {
+  return `
+  <a href="photograph.html?likes=${media.likes}"> <span class="portfolio-likes">${media.likes} ❤</span> </a> 
+  `;
 });
-
-numberOfLikes.addEventListener("click", getLikes);
+getLikes.sort(getLikes);
+//console.log(getLikes);
 
 const numberOfLikes = ["2", "6", "8", "5"];
 numberOfLikes.sort();
 console.log(numberOfLikes);
 
-function sortLikes() {
-  return numberOfLikes;
-}
-numberOfLikes.addEventListener("click", sortLikes);
-
-let numberOfLikes = ["media"];
-let likes = 0;
-const like = +new URLSearchParams(window.location.search).get("likes");
-const getLikes = (media) => {
-  return convertStringToHTML(
-    media.likes
-      .map((media) => {
-        return `<span class="portfolio-likes">${media.likes} ❤</span>  `;
-      })
-      .join("")
-  );
-};
-numberOfLikes.sort();
-console.log(numberOfLikes);
-
-const sortedLikes = numberOfLikes.sort(function (c1, c2) {
-  if (c1.xxx > c2.xxx) {
-    return 1;
-  } else {
-    return -1;
-  }
-});
-const byPopularity = document.querySelector(".sort-popularity");
-const byTitle = document.querySelector(".sort-title");
-const byDate = document.querySelector(".sort-date");
-
-byPopularity.addEventListener("click");
-byTitle.addEventListener("click");
-//byDate.addEventListener("click");
-
-const like = +new URLSearchParams(location.search).get("likes");
-const title = +new URLSearchParams(location.search).get("title");
-const date = +new URLSearchParams(location.search).get("date");
-
-const likes = { media: "likes" };
-
-const likes = like;
-const getTitle = {
-  media: "title",
-};
-
-/*const getLikes = (media, likes) => {
-  return `<span class="portfolio-likes">${media.likes} ❤</span>  `;
-};*/
-
 loadData().then((data) => {
   if (like) {
     return data.media.sort((media) => media.likes.includes(like));
   }
   return data.media;
 });
-
-const getLikes = (media, likes) => {
-  if (media.likes) {
-    return getLikes(media, likes);
-  }
-};
-byPopularity.addEventListener("click", getLikes);
-
-const getLikes = (media, likes) => {
-  return `<span class="portfolio-likes">${media.likes} ❤</span>  `;
-};
-const byPopularity = document.querySelector(".sort-popularity");
-byPopularity.addEventListener("click", () => {
-  //1 - recover number of likes
-  const like = +new URLSearchParams(location.search).get("likes");
-  console.log(like);
-  const likes = { media: "likes" };
-  console.log(likes);
-});
-const likes = { media: "likes" };
-
-for (key in title) console.log(title[key]);
-const like = new URLSearchParams(location.search).get("likes");
-loadData().then((data) => {
-  if (like) {
-    return data.medias.sort((media) => media.likes.includes(like));
-  }
-  return data.medias;
-});
-//let sortList=
-const byPopularity = document.querySelector(".sort-popularity");
-const like = +new URLSearchParams(window.location.search).get("likes");
-const likes = { media: "likes" };
-
-likes.sort(function (a, b) {
-  return;
-});
-
-byPopularity.addEventListener("click", () => {
-  //1 - recover number of likes
-  const like = +new URLSearchParams(window.location.search).get("likes");
-  const likes = { media: "likes" };
-  likes.sort((a, b) => a - b);
-  console.log(likes);
-  if (likes) {
-    return convertStringToHTML(
-      like.likes
-        .sort((likes) => {
-          return `<span class="portfolio-likes">${media.likes} ❤</span>  `;
-        })
-        .join("")
-    );
-  }
-  const titleList = [{ media: "title" }];
-
-  let listTitleContainer = document.querySelector(".sort-title");
-  let title = e.target.getAttribute(".title");
-
-  listTitleContainer.innerHTML = titleList[title].innerHTML;
-
-  const getLikes = (media) => {
-    return convertStringToHTML(
-      media.likes
-        .map((likes) => {
-          return `<span class="portfolio-likes">${media.likes} ❤</span>  `;
-        })
-        .join("")
-    );
-  };
-});
-//console.log(likes);
-
-const date = [{ media: "date" }];
-date.sort(function (a, b) {
-  // Turn your strings into dates, and then subtract them
-  // to get a value that is either negative, positive, or zero.
-  return new Date(b.date) - new Date(a.date);
-});
-
-const byDate = document.querySelector(".sort-date");
-byDate.addEventListener("click", sort);
-const date = [{ date: "date" }];
-date.sort(function (a, b) {
-  // Turn your strings into dates, and then subtract them
-  // to get a value that is either negative, positive, or zero.
-  return new Date(b.date) - new Date(a.date);
-});
-console.log(date);
 
 const date = [{ media: "date" }];
 const sortByDate = (date) => {
@@ -277,47 +92,6 @@ const sortByDate = (date) => {
 sortByDate(date);
 console.log(date);
 
-const byTitle = document.querySelector(".sort-title");
-
-let listTitleContainer = document.querySelector(".sort-title");
-const title = "$.title";
-
-const titleList = title.length;
-
-const titleSort = function sortByTitle(a, b) {
-  if (a > b) return 1;
-  if (b > a) return -1;
-
-  return length;
-  listTitleContainer.innerHTML = titleSort[title].innerHTML;
-};
-byTitle.addEventListener("click", titleSort);
-title.sort(titleSort);
-console.log(titleList);
-
-byTitle.addEventListener("click", () => {
-  // console.log(title);
-});
-
-const title = [{ media: "title" }];
-console.log(title);
-
-const sortList = [{ media: "likes", media: "title", media: "date" }];
-const listLikes = { media: "likes".length };
-console.log(listLikes);
-const sortList = [{ media: "likes", media: "title", media: "date" }];
-const likes = +new URLSearchParams(window.location.search).get("likes");
-const listLikes = [{ media: "likes" }];
-console.log(listLikes);
-
-var array = [
-  {
-    media: "title",
-    media: "likes",
-    media: "date",
-  },
-];
-
 function GetSortOrder(prop) {
   return function (a, b) {
     if (a[prop] > b[prop]) {
@@ -328,111 +102,3 @@ function GetSortOrder(prop) {
     return 0;
   };
 }
-array.sort(GetSortOrder("title"));
-const likes = +new URLSearchParams(location.search).get("likes");
-console.log(likes);
-const sortLikes = { media: "likes" };
-function GetSortOrder(prop) {
-  return function (a, b) {
-    if (a[prop] > b[prop]) {
-      return 1;
-    } else if (a[prop] < b[prop]) {
-      return -1;
-    }
-    return 0;
-  };
-}
-const getLikes = (media) => {
-  return convertStringToHTML(
-    media.likes
-      .map((media) => {
-        return `<span id="portfolio-likes">${media.likes} ❤</span>  `;
-      })
-      .join("")
-  );
-};
-<div class="like">
-  $
-  {media.likes
-    .map(
-      (like) =>
-        `<span> <a href="index.html?like=${media.likes}">#${like}</a></span> `
-    )
-    .join("")}
-</div>;
-const like = new URLSearchParams(window.location.search).get("like");
-const likes = document.getElementById("portfolio-likes");
-sortLikes.sort(GetSortOrder("likes"));
-console.log(sortLikes);
-<span class="title">${media.title}</span>
-const title = ["a", "c", "b", "e", "d"];
-const sortedTitle = title.sort();
-console.log(sortedTitle);
-
-const getLike = (media) => {
-  return `<span class="portfolio-likes">${media.likes} ❤</span>  `;
-};
-<ul class="like">
-  $
-  {photographer.likes
-    .map(
-      (like) =>
-        `<span> <li><a href="index.html?like=${media.likes}">#${media.likes}</a></li></span> `
-    )
-    .join("")}
-</ul>;
-
-
-
-const getTags = (photographer) => {
-  return convertStringToHTML(
-    photographer.tags
-      .map((tag) => {
-        return `<span> <li><a href="#">#${tag}</a></li></span> `;
-      })
-      .join("")
-  );
-};
-const getLikes = (media) => {
-  return convertStringToHTML(
-    media.likes
-      .map((likes) => {
-        return `<span> <a href="index.html?like=${media.likes}">#${like}</a></span> `;
-      })
-      .join("")
-  );
-};
-
-const likes = new URLSearchParams(window.location.search).get("likes");
-loadData()
-  .then((data) => {
-    if (likes) {
-      return data.media.filter((likes) =>
-        photographer.likes.includes(like)
-      );
-    }
-    return data.photographers;
-  });
-  
-  // sort
-  const byTitle = document.querySelector(".sort-title");
-  const title = new URLSearchParams(window.location.search).getAll("title");
-  const getTitle = (media) => {
-    return convertStringToHTML(
-      media.title
-        .map((title) => {
-          return ` <span class="title">${media.title}</span>`;
-        })
-        .join("")
-    );
-  };
-  //console.log(title);
-
-  //const like = new URLSearchParams(window.location.search).get("likes");
-  loadData().then((media) => {
-    if (title) {
-      //   return data.media.filter((title) => media.title.includes(title));
-    }
-    return data.media;
-  });
-  byTitle.addEventListener("click", loadData);
